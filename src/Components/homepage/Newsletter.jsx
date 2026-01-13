1f4dd8cf759bd99cd16f3d3c308c59477dd9147e
Maleek <abdulmalikasimiyu@gmail.com>
2026-01-13 15:27:33 +0100

    fixed deploy problem

--- src/Components/homepage/Newsletter.jsx
+++ src/Components/homepage/Newsletter.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "../../Css/newsletter.css";
+import "../../Css/Newsletter.css";
 
 const Newsletter = () => {
   const [email, setEmail] = useState("");
